8396b7fa5339069b5301dd8d1478e4c075938798
mthakur18-hub <75472634+mthakur18-hub@users.noreply.github.com>
2021-02-20 14:54:28 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+
+print("Client Server...")
+
+s = socket.socket()
+
+s_host = input("Enter server IP address: ")
+name = input("Enter your name: ")
+
+port = 6969
+
+print("Trying to connect to server...")
+
+s.connect((s_host, port))
+print("connected...")
+
+s.send(name.encode())
+
+s_name = s.recv(1024)
+s_name = s_name.decode()
+print(f"{s_name} has joined...")
+print("Enter !bye to exit.")
+
+
+connected = True
+while connected:
+    message = s.recv(1024)
+    message = message.decode()
+    print(s_name, '> ', message)
+    
+    message = input("You >")
+    if message == "!bye":
+        message = "See you next time..."
+        s.send(message.encode())
+        connected = False
+    s.send(message.encode())
