23881b5690953c5de066072f2511f24550a3880d
mthakur18-hub <75472634+mthakur18-hub@users.noreply.github.com>
2021-02-22 23:02:38 +0530

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,18 +1,17 @@
 import socket
 
-from tkinter import *
-
 import threading
 
 import sys
 
+import tqdm
+
+import os
+
+BUFFER = 1024
+
 
 class Server:
-    window = Tk()
-
-    window.title("chat app")
-
-
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -30,7 +29,7 @@ class Server:
 
         while True:
 
-            data = c.recv(20)
+            data = c.recv(BUFFER)
 
             print(str(data, 'utf-8'))
 
@@ -38,14 +37,29 @@ class Server:
     def send_msg(self, c, a, name):  #for sending messages
         while True:
             msg = input("")
-            s_msg = name + ': ' + msg
-            c.send(bytes(s_msg, 'utf-8'))
+
+            if msg == "!send":
+
+                sender = FtClient()
+
+            elif msg == "!accept":
+
+                receiver = FtServer()
+
+
+            else:
+
+                s_msg = name + ': ' + msg
+                
+                c.send(bytes(s_msg, 'utf-8'))
 
     def run(self):
 
         name = input("enter your name : ")
 
         print("Listening for connections...")
+
+        print("you can send files by typing !send and accept files by typing !accept")
 
         running = True
 
@@ -65,8 +79,6 @@ class Server:
 
             iThread.start()
 
-            if 
-
             print(str(a[0]) + ':' + str(a[1]), " connected.")
 
 
@@ -79,8 +91,20 @@ class Client:
 
         while connected:
             msg = input("")
-            s_msg = name + ': ' + msg
-            self.sock.send(bytes(s_msg, 'utf-8'))
+
+            if (msg == "!send"):
+
+                sender = FtClient()
+
+            elif msg == "!accept":
+
+                receiver = FtServer()
+
+            else:
+            
+                s_msg = name + ': ' + msg
+                
+                self.sock.send(bytes(s_msg, 'utf-8'))
 
 
     def __init__(self, address):
@@ -88,6 +112,10 @@ class Client:
         name = input("Enter your name : ")
 
         self.sock.connect((address, 1234))
+
+        print(f"connected to {address}")
+
+        print("you can send files by typing !send and accept files by typing !accept")
 
         connected = True
 
@@ -101,82 +129,117 @@ class Client:
 
         while connected:
 
-            data = self.sock.recv(20)
+            data = self.sock.recv(BUFFER)
 
             print(str(data, 'utf-8'))
 
 
-class TServer:
-
-    def __init__(self, port, filename):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.filename = filename
-
-        self.port = port
-
-        self.file = open(filename, 'rb')
-
-        self.filedata = self.file.read(1024)
-
-        self.sock.bind(('0.0.0.0', self.port))
-
-        self.sock.listen(1)
-
-        while True:
-
-            c , a = self.sock.accept()
-        
-            c.send(self.filedata)
-
-            c.close()
-
-            break
-
-        print("file has been sent sucessfully...")
-
-
-class TClient:
+class FtServer:
     
-    def __init__(self, address, port, filename):
+    def __init__(self):
+
+        SERVER_HOST = "0.0.0.0"
+        SERVER_PORT = 5001
         
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        BUFFER_SIZE = 1024
+        SEPARATOR = "<SEPARATOR>"
+        
 
-        self.filename = filename
+        s = socket.socket()
 
-        self.sock.connect((address, 1234))
+        s.bind((SERVER_HOST, SERVER_PORT))
 
-        while True:
+        s.listen(5)
+        print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-            file = open(filename, 'wb')
+        client_socket, address = s.accept() 
+
+        print(f"[+] {address} is connected.")
+
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        
+        filename, filesize = received.split(SEPARATOR)
+
+        filename = os.path.basename(filename)
+
+        location = input("Enter complete path to where you want to save the file : ")
+
+        file = location + "\\" + filename
+
+        filesize = int(filesize)
+
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(file, "wb") as f:
+            while True:
+                
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                
+                if not bytes_read:    
+                    
+                    break
+                
+                f.write(bytes_read)
+                
+                progress.update(len(bytes_read))
+
+
+        client_socket.close()
+
+        s.close()
+
+
+class FtClient:
+
+    def __init__(self):
+
+        SEPARATOR = "<SEPARATOR>"
             
-            file_data = self.sock.recv(1024)
+        BUFFER_SIZE = 1024
+            
+        host = input("Enter receiver's ip address: ")
+        
+        port = 5001
+        
+        filename = input("Enter file name : ")
+        
+        filesize = os.path.getsize(filename)
+        
+        s = socket.socket()
+        
+        print(f"Connecting to {host}:{port}")
+        
+        s.connect((host, port))
+        
+        print("Connected.")
+        
+        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+        
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        
+        with open(filename, "rb") as f:
+            while True:
+                
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    
+                    break
+                
+                s.sendall(bytes_read)
+                
+                progress.update(len(bytes_read))
+        
+        s.close()
 
-            file.write(file_data)
 
-            file.close()
 
-            break
-
-        print("file has been received sucessfully...")
-
-#for sending = python xyz.py port filename
-#for receiving = python xyz.py ip port filename
 
         
-if (len(sys.argv) == 2):
+if (len(sys.argv) > 1):
 
     client = Client(sys.argv[1])
 
-elif (len(sys.argv) == 1):
+else:
 
     server = Server()
 
     server.run()
-
-elif (len(sys.argv) == 3):
-    
-    sender = TServer(int(sys.argv[1]), sys.argv[2])
-
-else:
-    receiver = TClient(sys.argv[1], int(sys.argv[2]), sys.argv[3])
